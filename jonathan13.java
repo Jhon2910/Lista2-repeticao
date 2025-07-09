ae11df729eb5641424dae62ee44ea46f76a12ccc
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-09 11:06:59 -0300

    Update jonathan13.java

--- jonathan13.java
+++ jonathan13.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class jonathan13 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero: ");
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int primo = 0;
+
+            while (primo <= n) {
+                if (primo % 0) {
+                    System.out.println(primo+ "Não primo! ");
+                } else {
+                    System.out.println(primo+ "primo! ");
+                }
+                n++;
+            }
+        }
+    }
+}//nao terminei
