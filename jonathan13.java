4e47e8ec9baff106ef5dd31e2b8f84d870d619b5
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-10 21:55:28 -0300

    Update jonathan13.java

--- jonathan13.java
+++ jonathan13.java
@@ -1,22 +1,41 @@
 import java.util.Scanner;
 
 public class jonathan13 {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n;
+        int i = 0;
 
-        System.out.println("Digite um numero: ");
-        if (sc.hasNextInt()) {
-            int n = sc.nextInt();
-            int primo = 0;
-
-            while (primo <= n) {
-                if (primo % 0) {
-                    System.out.println(primo+ "Não primo! ");
-                } else {
-                    System.out.println(primo+ "primo! ");
-                }
-                n++;
+        while (i < Integer.MAX_VALUE) {
+            System.out.print("\nDigite um número para saber se é primo ou não: ");
+            while (!sc.hasNextInt()) {
+                System.out.print("\nErro! Você não digitou um número válido! Tente novamente.");
+                System.out.print("\nDigite um número para saber se é primo ou não: ");
+                sc.next();
             }
+
+            n = sc.nextInt();
+            sc.nextLine();
+
+            if (n <= 1) {
+                System.out.printf("O número %d não é primo.\n", n);
+            } else {
+                boolean primo = true;
+
+                for (int j = 2; j <= Math.sqrt(n); j++) {
+                    if (n % j == 0) {
+                        primo = false;
+                        break;
+                    }
+                }
+                if (primo) {
+                    System.out.printf("O número %d é primo.\n", n);
+                } else {
+                    System.out.printf("O número %d não é primo.\n", n);
+                }
+            }
+            i++;
         }
+        sc.close();
     }
-}//nao terminei
+}
