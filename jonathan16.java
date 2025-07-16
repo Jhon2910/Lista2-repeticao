fae43faae58a2b008687cfa32699f7eaee8df665
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-16 20:40:32 -0300

    Update jonathan16.java

--- jonathan16.java
+++ jonathan16.java
@@ -4,24 +4,54 @@ public class jonathan16 {
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um numero binario(0's e 1's) para obter decimal,octal e hexadecimal: ");
-        int binario = sc.nextInt();
+        int binario,i = Integer.MIN_VALUE;
+        int sair;
 
-        double decimal = 0;
-        int expoente = 0;
+        while (i < Integer.MAX_VALUE){
 
-        while (binario != 0) {
-            int dig = binario % 10;
-            binario = binario / 10;
-            if (dig != 0 && dig != 1) {
-                System.out.printf("O numero %d não é binario!\n", binario);
+            System.out.print("1 para sair ou qualquer outro caracrter para continuar: ");
+            sair = sc.nextInt();
+
+            if (sair == 1){
                 return;
-            } else {
-                while (decimal <= 0){
-                    decimal = dig * Math.pow(2, expoente);
-                    expoente++;
+            }
+
+            else {
+
+                System.out.print("\nDigite um numero binario(0's e 1's) para obter decimal,octal e hexadecimal: ");
+                while (!sc.hasNextInt()) {
+                    System.out.println("\ndigite um numero valido!");
+                    System.out.print("Digite um numero binario(0's e 1's) para obter decimal,octal e hexadecimal: ");
+                    sc.next();
                 }
-                System.out.printf("decimal = %f\n",decimal);
+                binario = sc.nextInt();
+
+                double decimal = 0;
+                int expoente = 0;
+
+                while (binario != 0) {
+                    int dig = binario % 10;
+                    binario = binario / 10;
+                    if (dig != 0 && dig != 1) {
+                        System.out.printf("O numero %d não é binario!\n", binario);
+                        return;
+                    } else {
+                        while (decimal <= 0) {
+                            decimal = dig * Math.pow(2, expoente);
+                            expoente++;
+                        }
+                        System.out.printf("decimal = %f\n", decimal);
+
+
+                        System.out.print("1 para sair ou qualquer outro caracrter para continuar: ");
+                        sair = sc.nextInt();
+                        if (sair == 1) {
+                            return;
+                        } else {
+                            i++;
+                        }
+                    }
+                }
             }
         }
     }
