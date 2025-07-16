a75a7a633ac58e2d32ff373d6694e62ae3be7096
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-16 11:11:51 -0300

    Update jonathan17.java

--- jonathan17.java
+++ jonathan17.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class jonathan17{
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
-        double soma,sub,mult,div,n,resposta;
-        int i,repetir=0,sair,opção,quantidade;
-
+        double soma=0,sub=0,mult=0,div = 0,n,resposta;
+        int i,repetir=0,sair, op,quantidade;
 
         while (repetir < Integer.MAX_VALUE){
 
@@ -26,54 +25,47 @@ public class jonathan17{
             quantidade = sc.nextInt();
         }
 
-        System.out.print("Digite os numeros que gostaria de calcular: ");
-        while (!sc.hasNextInt()){
-            System.out.print("numero inválido");
-            System.out.print("Digite os numeros que gostaria de calcular: ");
-            if (quantidade >= 1 && quantidade <= 5){
-        }
-
-        System.out.print("--------------Menu----------------");
-        System.out.println("1 - soma");
-        System.out.println("2 - sub");
-        System.out.println("3 - mult");
-        System.out.println("4 - div");
-        System.out.println("5 - Sair");
-
-        System.out.print("\nDigite a opção: ");
-
-
-        while (opção < 1 || opção > 5 || opção == !sc.hasNextInt()){
-            System.out.println("\nNumero invalido!");
-            System.out.println("--------------Menu----------------");
-            System.out.println("1 - soma");
-            System.out.println("2 - sub");
-            System.out.println("3 - mult");
-            System.out.println("4 - div");
-            System.out.println("5 - Sair");
-            System.out.print("Digite a opção: ");
-            sc.next();
-        }
-        opção = sc.nextInt();
-
-        if (opção == 1){
-            System.out.printf("\nA soma = %.2f\",resposta");
-        }
-        else if(opção == 2){
-            System.out.printf("\nA sub = %.2f\",resposta");
-        }
-        else if(opção == 3){
-            System.out.printf("\nA mult = %.2f\",resposta");
-        }
-        else if (opção == 4){
-            System.out.printf("\nA div = %.2f\",resposta");
-        }
         else {
-            return;
-        }
-        }
+            System.out.print("Digite os numeros que gostaria de calcular: ");
+            while (!sc.hasNextInt()) {
+                System.out.print("numero inválido");
+                System.out.print("Digite os numeros que gostaria de calcular: ");
+                n = sc.nextDouble();
+                if (quantidade >= 1 && quantidade <= 5) {
+                }
 
-       repetir++;
+                System.out.print("--------------Menu----------------");
+                System.out.println("1 - soma");
+                System.out.println("2 - sub");
+                System.out.println("3 - mult");
+                System.out.println("4 - div");
+                System.out.println("5 - Sair");
+
+                System.out.print("\nDigite a opção: ");
+                op = sc.nextInt();
+                if (op < 1 || op > 5 || !sc.hasNextInt()) {
+                    System.out.print("Numero inválido!");
+                    return;
+                }
+
+                if (op == 1) {
+                    soma =+ n;
+                    System.out.printf("\nA soma = %.2f\",resposta", soma);
+                } else if (op == 2) {
+                    sub =- n;
+                    System.out.printf("\nA sub = %.2f\",resposta", sub);
+                } else if (op == 3) {
+                    System.out.printf("\nA mult = %.2f\",resposta", mult);
+                    mult *= n;
+                } else if (op == 4) {
+                    div /= n;
+                    System.out.printf("\nA div = %.2f\",resposta", div);
+                } else {
+                    return;
+                }
+            }
+            repetir++;
+        }
         }
     }
 }
