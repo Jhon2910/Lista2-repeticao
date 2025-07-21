4739f36edb686886bb9eb196c8a72ca3ead8b9d6
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-21 20:03:49 -0300

    Update jonathan13.java

--- jonathan13.java
+++ jonathan13.java
@@ -19,35 +19,34 @@ public class jonathan13 {
                 System.out.println("Finalizando!");
                 return;
             }
-            System.out.print("\nDigite um número para saber se é primo ou não: ");
-            while (!sc.hasNextInt()) {
-                System.out.print("\nErro! Você não digitou um número válido! Tente novamente.");
+
                 System.out.print("\nDigite um número para saber se é primo ou não: ");
-                sc.next();
-            }
-
-            n = sc.nextInt();
-            sc.nextLine();
-
-            if (n <= 1) {
-                System.out.printf("O número %d não é primo.\n", n);
-            } else {
-                boolean primo = true;
-
-                for (int j = 2; j <= Math.sqrt(n); j++) {
-                    if (n % j == 0) {
-                        primo = false;
-                        break;
-                    }
+                while (!sc.hasNextInt()) {
+                    System.out.print("\nErro! Você não digitou um número válido! Tente novamente.");
+                    System.out.print("\nDigite um número para saber se é primo ou não: ");
+                    sc.next();
                 }
-                if (primo) {
-                    System.out.printf("O número %d é primo.\n", n);
-                } else {
+
+                n = sc.nextInt();
+                sc.nextLine();
+
+                if (n <= 1) {
                     System.out.printf("O número %d não é primo.\n", n);
-                }
+                } else {
+                    boolean primo = true;
+
+                    for (int j = 2; j <= Math.sqrt(n); j++) {
+                        if (n % j == 0) {
+                            primo = false;
+                            break;
+                        }
+                    }
+                    if (primo) {
+                        System.out.printf("O número %d é primo.\n", n);
+                    } else {
+                        System.out.printf("O número %d não é primo.\n", n);
+                    }
+                }sc.close();
             }
-            i++;
         }
-        sc.close();
     }
-}
