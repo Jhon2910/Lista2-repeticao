251d4ef38a0df6df23bc1a4fa248ff635a403c55
Jonathan Rodrigues <91800109+Jhon2910@users.noreply.github.com>
2025-07-21 21:02:25 -0300

    Update jonathan16.java

--- jonathan16.java
+++ jonathan16.java
@@ -1,58 +1,96 @@
 import java.util.Scanner;
 
 public class jonathan16 {
-    public static void main(String[]args) {
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String binario;
+        int decimal = 0;
 
-        int binario,i = Integer.MIN_VALUE;
-        int sair;
+        System.out.print("Digite um número binário (com N bits): ");
+        binario = sc.nextLine();
 
-        while (i < Integer.MAX_VALUE){
-
-            System.out.print("1 para sair ou qualquer outro caracrter para continuar: ");
-            sair = sc.nextInt();
-
-            if (sair == 1){
-                return;
-            }
-
-            else {
-
-                System.out.print("\nDigite um numero binario(0's e 1's) para obter decimal,octal e hexadecimal: ");
-                while (!sc.hasNextInt()) {
-                    System.out.println("\ndigite um numero valido!");
-                    System.out.print("Digite um numero binario(0's e 1's) para obter decimal,octal e hexadecimal: ");
-                    sc.next();
-                }
-                binario = sc.nextInt();
-
-                double decimal = 0;
-                int expoente = 0;
-
-                while (binario != 0) {
-                    int dig = binario % 10;
-                    binario = binario / 10;
-                    if (dig != 0 && dig != 1) {
-                        System.out.printf("O numero %d não é binario!\n", binario);
-                        return;
-                    } else {
-                        while (decimal <= 0) {
-                            decimal = dig * Math.pow(2, expoente);
-                            expoente++;
-                        }
-                        System.out.printf("decimal = %f\n", decimal);
-
-
-                        System.out.print("1 para sair ou qualquer outro caracrter para continuar: ");
-                        sair = sc.nextInt();
-                        if (sair == 1) {
-                            return;
-                        } else {
-                            i++;
-                        }
-                    }
+        int validoFlag = 1;
+        int i = 0;
+        while (i < binario.length()) {
+            int asciiBit = binario.charAt(i);
+            if (asciiBit != 48) {
+                if (asciiBit != 49) {
+                    validoFlag = 0;
+                    i = binario.length();
                 }
             }
+            i++;
         }
+
+        if (validoFlag == 1) {
+            int j = 0;
+            while (j < binario.length()) {
+                int asciiBitChar = binario.charAt(binario.length() - 1 - j);
+                if (asciiBitChar == 49) {
+                    int p = 1;
+                    int k = 0;
+                    while (k < j) {
+                        p *= 2;
+                        k++;
+                    }
+                    decimal += p;
+                }
+                j++;
+            }
+
+            System.out.println("Número Binário: " + binario);
+            System.out.println("Em Decimal: " + decimal);
+
+            String octal = "";
+            int tempDecimalOctal = decimal;
+            if (tempDecimalOctal == 0) {
+                octal = "0";
+            } else {
+                int l = 0;
+                while (tempDecimalOctal > 0) {
+                    int resto = tempDecimalOctal % 8;
+                    int charRestoAscii = (int) (48 + resto);
+
+                    String tempOctal = "";
+                    tempOctal += (char)charRestoAscii;
+                    tempOctal += octal;
+                    octal = tempOctal;
+
+                    tempDecimalOctal /= 8;
+                }
+            }
+            System.out.println("Em Octal: " + octal);
+
+            String hexadecimal = "";
+            int tempDecimalHex = decimal;
+            if (tempDecimalHex == 0) {
+                hexadecimal = "0";
+            } else {
+                int m = 0;
+                while (tempDecimalHex > 0) {
+                    int resto = tempDecimalHex % 16;
+                    int charRestoAscii;
+                    if (resto < 10) {
+                        charRestoAscii = (int) (48 + resto);
+                    } else {
+                        charRestoAscii = (int) (65 + (resto - 10));
+                    }
+
+                    String tempHex = "";
+                    tempHex += (char)charRestoAscii;
+                    tempHex += hexadecimal;
+                    hexadecimal = tempHex;
+
+                    tempDecimalHex /= 16;
+                }
+            }
+            System.out.println("Em Hexadecimal: " + hexadecimal);
+
+        } else {
+            System.out.println("Erro: O valor informado não é um número binário válido.");
+        }
+
+        sc.close();
     }
 }
